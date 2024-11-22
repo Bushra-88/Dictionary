4daaf834d8ee973d9b9a55a4a6b08efc422832d2
Bushra Alhusaini <alhusaini.b2023@gmail.com>
2024-11-22 14:07:28 +0100

    install react and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import bushraLogo from "./assets/bushra-logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <img src={bushraLogo} className="logo" alt=" logo" />
+        <h1>Dictionary</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
